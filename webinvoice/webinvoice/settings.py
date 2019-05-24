2b3616d09eb72c9bede8de50a0599889baf7a722
root <ishikawasyuusaku@gmail.com>
2019-05-24 09:12:43 +0900

    a

--- webinvoice/webinvoice/settings.py
+++ webinvoice/webinvoice/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
+STATIC_URL = '/static_webinvoice/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'web/static'),
 ]
