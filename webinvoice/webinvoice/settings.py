d0d438a3055217aac7cbed53aef89416538c9439
root <root@os3-369-17689.vs.sakura.ne.jp>
2019-05-27 10:46:39 +0900

    a

--- webinvoice/webinvoice/settings.py
+++ webinvoice/webinvoice/settings.py
@@ -84,10 +84,19 @@ WSGI_APPLICATION = 'webinvoice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webinvoicedb',
+        'USER': 'root',
+        'PASSWORD': '332191-Aa',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        },
     }
 }
+
+
 
 NUMBER_GROUPING = 3
 # Logging
